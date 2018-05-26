4f80f48903db2161a47edefa065c29c45bfb2346
Greg Garabedian <ggarabedian09@gmail.com>
2018-05-26 17:42:33 -0400

    feat: push 'clean' airlines to array

--- src/app.js
+++ src/app.js
@@ -5,16 +5,19 @@ angular.module('App',["chart.js"])
   $scope.hello = "world";
   $scope.claims = [];
 
-  $scope.airlineClaimLoss = [
-    {
-      "Delta": [65, 59, 80, 81, 56, 55, 40, 30, 18, 24, 17, 55]
-    }
+  $scope.airlineMonthlyClaimLoss = [
+    // {"Delta": [65, 59, 80, 81, 56, 55, 40, 30, 18, 24, 17, 55]}
   ];
+
+  $scope.airportAvgMonthlyClaims = [
+
+  ]
 
   $scope.lineLabels = ["January", "February", "March", "April", "May", "June", "July","August","September","October","November","December"];
   $scope.lineSeries = ['Series A', 'Series B'];
   $scope.lineData = [
-    $scope.airlineClaimLoss[0]["Delta"],
+    // $scope.airlineClaimLoss[0]["Delta"],
+    [78, 28, 10, 36, 77, 77, 60, 20, 18, 44, 27, 85],
     [28, 48, 40, 19, 86, 27, 90, 30, 28, 14, 37, 75]
   ];
 
@@ -22,12 +25,17 @@ angular.module('App',["chart.js"])
     Papa.parse(data,{
       header: true,
       step: function(results,parser){
-        console.log("row data: ", results.data[0]);
+        // console.log("row data: ", results.data[0]);
+        let airline = results.data[0]["Airline Name"].trim()
+        if ($scope.airlineMonthlyClaimLoss.indexOf(airline) === -1 && airline.length > 1){
+          $scope.airlineMonthlyClaimLoss.push(airline)
+        }
       },
       complete: function(){
         console.log("done");
       }
     })
+    debugger
   }
 
   function getClaims(){
