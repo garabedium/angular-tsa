2fc8ed40d1dfa22be6ad9a64eee8528a74d81707
Greg Garabedian <ggarabedian09@gmail.com>
2018-05-26 16:59:57 -0400

    feat: building line graph using scoped data

--- src/app.js
+++ src/app.js
@@ -3,53 +3,20 @@ angular.module('App',["chart.js"])
   .controller('MainCtrl',function($scope,$http){
 
   $scope.hello = "world";
-  $scope.presidents = [];
   $scope.claims = [];
 
-
-
-  $scope.pieLabels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-  $scope.pieData = [300, 500, 100];
-
-  $scope.categories = [
-    {"id": 0, "name": "Development"},
-    {"id": 1, "name": "Design"},
-    {"id": 2, "name": "Exercise"},
-    {"id": 3, "name": "Humor"}
+  $scope.airlineClaimLoss = [
+    {
+      "Delta": [65, 59, 80, 81, 56, 55, 40, 30, 18, 24, 17, 55]
+    }
   ];
 
-  $scope.bookmarks = [
-    {"id": 0, "title": "AngularJS", "url":"http://angularjs.org", "category":"Development"},
-    {"id": 1, "title": "React", "url":"http://react.org", "category":"Development"},
-    {"id": 2, "title": "Vue", "url":"http://vuejs.org", "category":"Development"},
-    {"id": 3, "title": "New Yorker", "url":"http://newyorker.org", "category":"Humor"},
-    {"id": 4, "title": "Dilbert", "url":"http://dilbert.org", "category":"Humor"},
-    {"id": 5, "title": "Men's Health", "url":"http://menshealth.org", "category":"Exercise"}
+  $scope.lineLabels = ["January", "February", "March", "April", "May", "June", "July","August","September","October","November","December"];
+  $scope.lineSeries = ['Series A', 'Series B'];
+  $scope.lineData = [
+    $scope.airlineClaimLoss[0]["Delta"],
+    [28, 48, 40, 19, 86, 27, 90, 30, 28, 14, 37, 75]
   ];
-
-  $scope.currentCategory = null;
-
-  function setCurrentCategory(category){
-    $scope.currentCategory = category;
-  }
-
-  function isCurrentCategory(category){
-    return $scope.currentCategory !== null && category.name === $scope.currentCategory.name;
-  }
-
-  function hasPresidents(){
-    return $scope.presidents.length > 0
-  }
-
-  function getPresidents(){
-    $http({
-      method: 'GET',
-      url: 'src/data/presidents.json'
-    }).then(function successCallback(response) {
-      $scope.presidents = response.data;
-    }, function errorCallback(response) {
-    });
-  }
 
   function streamCSV(data){
     Papa.parse(data,{
@@ -74,10 +41,6 @@ angular.module('App',["chart.js"])
     });
   }
 
-  $scope.setCurrentCategory = setCurrentCategory;
-  $scope.isCurrentCategory = isCurrentCategory;
-  $scope.getPresidents = getPresidents;
-  $scope.hasPresidents = hasPresidents;
   $scope.getClaims = getClaims;
 
 })
